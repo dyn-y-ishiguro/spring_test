1da0cd25e72147474e5ab8f86dc3e30b572b7a67
yishiguro <yishiguro@valuecommerce.co.jp>
2021-04-28 09:53:23 +0900

    use CookieLocaleResolver

--- src/main/java/com/example/hello/Application.java
+++ src/main/java/com/example/hello/Application.java
@@ -1,13 +1,19 @@
 package com.example.hello;
 
+import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 @SpringBootApplication
-public class Application {
+public class Application implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -20,4 +26,25 @@ public class Application {
 		messageSource.setDefaultEncoding("UTF-8");
 		return messageSource;
 	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+		cookieLocaleResolver.setCookieName("locale");
+		cookieLocaleResolver.setDefaultLocale(Locale.JAPAN);
+		return cookieLocaleResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
 }
