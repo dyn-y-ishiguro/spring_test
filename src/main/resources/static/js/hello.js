e23a322a3c7d5618426e8e3a723cbee964fa8997
yishiguro <yishiguro@valuecommerce.co.jp>
2021-05-21 11:14:51 +0900

    js test

---
+++ src/main/resources/static/js/hello.js
@@ -0,0 +1,15 @@
+function hello1() {
+    console.log("hello1!");
+};
+document.getElementById("hello1").onclick = function() {
+  hello1();
+};
+
+function hello2() {
+    console.log("hello2!");
+};
+window.hello2 = hello2;
+
+window.hello3 = function() {
+    console.log("hello3!");
+};
