7f5b327cf71e330689ffce858f5f6ccd99ce9055
yishiguro <yishiguro@valuecommerce.co.jp>
2021-06-03 20:20:20 +0900

    use i18next-browser-languagedetector

--- src/main/js/hello.js
+++ src/main/js/hello.js
@@ -1,6 +1,7 @@
 import './common.js';
 import Vue from 'vue';
 import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import VueI18Next from '@panter/vue-i18next';
 
 function hello1() {
@@ -19,39 +20,26 @@ window.hello3 = function() {
   console.log(i18next.t("greet3"));
 };
 
-function GetCookie(name) {
-  var result = null;
-
-  var cookieName = name + '=';
-  var allcookies = document.cookie;
-
-  var position = allcookies.indexOf(cookieName);
-  if(position != -1) {
-    var startIndex = position + cookieName.length;
-
-    var endIndex = allcookies.indexOf(';', startIndex);
-    if(endIndex == -1) {
-      endIndex = allcookies.length;
-    }
-
-    result = decodeURIComponent(allcookies.substring(startIndex, endIndex));
-  }
-
-  return result;
-}
-
 // for i18next
 //window.i18next = i18next;
 
-i18next.init(
+const languageDetectorOption = {
+  order: ['querystring', 'cookie', 'navigator'],
+  lookupQuerystring: 'lang',
+  lookupCookie: 'locale',
+  caches: [],
+};
+
+i18next.use(LanguageDetector).init(
   {
     fallbackLng: 'ja',
-    resources: {}
+    resources: {
+      ja: {'translation': ja_dic},
+      en: {'translation': en_dic},
+    },
+    detection: languageDetectorOption,
   }
 );
-i18next.addResources('ja', 'translation', ja_dic);
-i18next.addResources('en', 'translation', en_dic);
-i18next.changeLanguage(GetCookie('locale'));
 
 Vue.use(VueI18Next);
 const i18n = new VueI18Next(i18next);
