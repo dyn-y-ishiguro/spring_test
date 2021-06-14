8446d3854f7c05bf26cd94afb55fba5db7703a40
yishiguro <yishiguro@valuecommerce.co.jp>
2021-06-14 21:25:29 +0900

    create MyCookieLocaleResolver

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,5 +1,10 @@
 package com.example.hello;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +12,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 	@GetMapping("/")
-	public String hello(Model model) {
+	public String hello(Model model, HttpServletRequest request) {
+		String hotelCode = request.getParameter("hotelCode");
+		LogUtils.error("hotelCode: " + hotelCode);
+		TranslationProject tl = TranslationProject.getTranslationProject(hotelCode);
+		LogUtils.error("tl.supportLanguage: " + Arrays.toString(tl.supportLanguage.toArray()));
+		Map<String,String> supportLanguageMap = new HashMap<>();
+		supportLanguageMap.put("ja", "日本語");
+		for(String sl : tl.supportLanguage){
+			if ("en".equals(sl)) {
+				supportLanguageMap.put("en", "English");
+			}
+			else if ("zh-CN".equals(sl)) {
+				supportLanguageMap.put("zh-CN", "简体中文");
+			}
+			else if ("zh-TW".equals(sl)) {
+				supportLanguageMap.put("zh-TW", "繁體中文");
+			}
+			else if ("ko".equals(sl)) {
+				supportLanguageMap.put("ko", "한국어");
+			}
+		}
+
 		model.addAttribute("message", "Hello Thymeleaf!!");
 		model.addAttribute("name", "taro");
+		model.addAttribute("supportLanguageMap", supportLanguageMap);
 		return "hello";
+	}
+
+	@GetMapping("/link")
+	public String link(Model model) {
+		return "link";
 	}
 }
---
+++ src/main/java/com/example/hello/TranslationProject.java
@@ -0,0 +1,38 @@
+package com.example.hello;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class TranslationProject {
+	public String hotelCode;
+	public List<String> supportLanguage;
+
+	public static TranslationProject getTranslationProject(String hotelCode) {
+		TranslationProject translationProject = new TranslationProject();
+
+		List<String> sl = new ArrayList<>();
+		if ("0000000001".equals(hotelCode)) {
+			// 多言語未対応
+		}
+		else if ("0000000002".equals(hotelCode)) {
+			// 多言語対応 英語のみ
+			sl.add("en");
+		}
+		else if ("0000000003".equals(hotelCode)) {
+			// 多言語対応 韓国語のみ
+			sl.add("ko");
+		}
+		else if ("0000000004".equals(hotelCode)) {
+			// 多言語対応 すべて
+			sl.add("en");
+			sl.add("zh-CN");
+			sl.add("zh-TW");
+			sl.add("ko");
+		}
+
+		translationProject.hotelCode = hotelCode;
+		translationProject.supportLanguage = sl;
+
+		return translationProject;
+	}
+}
